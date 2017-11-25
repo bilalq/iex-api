8ce1a012d541f851161cf88102f8b6de0a280e08
Bilal Quadri <bilalquadri92@gmail.com>
2017-11-25 04:28:19 -0500

    Define module for info on API attribution
    The IEX API has guidelines on attribution information to use if their
    data is redistributed. This module makes it easy to discover and
    reference that information.
    
    See: https://iextrading.com/developer/docs/#attribution

---
+++ src/attribution.ts
@@ -0,0 +1,10 @@
+/**
+ * Exports information that is required to provide IEX attribution as listed in
+ * their API documentation: https://iextrading.com/developer/docs/#attribution
+ */
+export default {
+  citation: 'Data provided for free by IEX.',
+  link: 'https://iextrading.com/developer',
+  termsOfServiceLink: 'https://iextrading.com/api-exhibit-a',
+  TOPSPriceDataCitation: 'IEX Real-Time Price'
+}
