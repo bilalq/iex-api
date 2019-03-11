f74020c1c56474787d7c1ac004db2118ec68ee5f
Mike Mishkin <mishkin.mike@gmail.com>
2019-03-11 09:40:23 -0500

    linter corrections

--- src/client.ts
+++ src/client.ts
@@ -288,12 +288,12 @@ export default class IEXClient {
   }
 
    /**
-   * Returns an array of quote objects for a given collection type. Currently supported collection types are sector, tag, and list
-   *
-   * @see https://iextrading.com/developer/docs/#collections
-   * @param type Type of collection
-   * @param collectionName Name of the sector, tag, or list to return and is case sensitive. 
-   */
+    * Returns an array of quote objects for a given collection type. Currently supported collection types are sector, tag, and list
+    *
+    * @see https://iextrading.com/developer/docs/#collections
+    * @param type Type of collection
+    * @param collectionName Name of the sector, tag, or list to return and is case sensitive.
+    */
   public stockMarketCollection(type: StocksAPI.Collection, collectionName: string): Promise<StocksAPI.QuoteResponse[]> {
     return this.request(`/stock/market/collection/${type}?collectionName=${collectionName}`)
   }
