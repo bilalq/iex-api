7358282c9140c29b0c5c83b407e1d229f25cf65f
Derek Watring <dwatring4@hotmail.com>
2019-11-07 16:48:26 -0600

    strict undefined definition (#30)

--- src/client.ts
+++ src/client.ts
@@ -16,7 +16,7 @@ const formatDate = (date: Date | undefined) => date !== undefined ? `${date.getF
 // tslint:disable:no-unsafe-any
 const toParams = (params: any): string =>
   Object.keys(params)
-    .filter(param => param !== undefined)
+    .filter((param: string | undefined) => param !== undefined)
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
     .join('&')
 // tslint:enable:no-unsafe-any
